63004e066ad348d6a77cc05c6211b0d4d7b621e7
Sophie Gu <sophieqgu@gmail.com>
2020-12-04 03:48:03 +0000

    Automatically backed up by Learn

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -7,43 +7,6 @@ class DoctorsController < ApplicationController
     @doctor = Doctor.find(params[:id])
   end
 
-  def new
-    @Doctor = Doctor.new
-  end
-
-  def create
-    @doctor = Doctor.new(artist_params)
-
-    if @artist.save
-      redirect_to @artist
-    else
-      render :new
-    end
-  end
-
-  def edit
-    @artist = Artist.find(params[:id])
-  end
-
-  def update
-    @artist = Artist.find(params[:id])
-
-    @artist.update(artist_params)
-
-    if @artist.save
-      redirect_to @artist
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @artist = Artist.find(params[:id])
-    @artist.destroy
-    flash[:notice] = "Artist deleted."
-    redirect_to artists_path
-  end
-
   private
 
   def artist_params
